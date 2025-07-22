43ec8b6adf159ceb3db4c087590cbd37bcf4aa48
Shiv1024 <s_sinha@me.iitr.ac.in>
2025-07-22 16:12:03 +0530

    user bean added

---
+++ POS/src/com/flipfit/beans/User.java
@@ -0,0 +1,67 @@
+package com.flipfit.beans;
+
+public class User {
+    private int userId;
+    private int userPhoneNumber;
+    private String userName;
+    private String userEmail;
+    private String userAddress;
+    private String userPassword;
+    private String userRole;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(int userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}

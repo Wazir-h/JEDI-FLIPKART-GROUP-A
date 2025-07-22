4183da1331abed6c1ed5dc7aa606ca329d56cfac
Shiv1024 <s_sinha@me.iitr.ac.in>
2025-07-22 17:32:09 +0530

    BookingId added

---
+++ POS/src/com/flipfit/beans/Booking.java
@@ -0,0 +1,13 @@
+package com.flipfit.beans;
+
+public class Booking extends Slot{
+   private int bookingId;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+}

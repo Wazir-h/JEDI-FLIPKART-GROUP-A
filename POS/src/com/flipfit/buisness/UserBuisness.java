bdbdeb17b3e44f3a0f3b17517a64f118f2b63e92
Shiv1024 <s_sinha@me.iitr.ac.in>
2025-07-22 16:35:32 +0530

    UserBuisness Added

---
+++ POS/src/com/flipfit/buisness/UserBuisness.java
@@ -0,0 +1,17 @@
+package com.flipfit.buisness;
+import  com.flipfit.buisness.UserBuisness;
+public class UserBuisness {
+    public void createProfile(){
+        System.out.println("Profile Created");
+    }
+    public void updateProfile(){
+        System.out.println("Profile updated");
+    }
+    public String getRole(){
+        System.out.println("Role:");
+        return "";
+    }
+    public void viewAllGyms(){
+        System.out.println("Display All Gyms");
+    }
+}

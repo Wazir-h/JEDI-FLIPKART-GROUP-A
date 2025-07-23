3dc8fa70e373b133e2fee0ad61bc4c72f8a45a04
Sujal Mishra <sujal.mishra.374@gmail.com>
2025-07-23 10:28:23 +0530

    Payment and Registration business added

---
+++ POS/src/com/flipfit/business/GymPaymentBusiness.java
@@ -0,0 +1,10 @@
+package com.flipfit.business;
+
+public class PaymentBusinessService {
+    public void makePayment(String customerId){}
+
+    public void retrievePaymentinfor(){}
+
+    public void retrievePayment(){}
+
+}
---
+++ POS/src/com/flipfit/business/RegistrationBusinessService.java
@@ -0,0 +1,14 @@
+package com.flipkart.business;
+
+public class RegistrationBusinessService {
+    public void getRegistrationID(){
+        System.out.println("This is Registration ID: ");
+    }
+    public void getRole(){
+        System.out.println("Role of User: ");
+    }
+
+    public void getDateofRegistration(){
+        System.out.println("Date of Registration: ");
+    }
+}

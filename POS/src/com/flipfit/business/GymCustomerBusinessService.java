37a6ebc1ab67774e34798756830901b4d6cac1d1
Shiv1024 <s_sinha@me.iitr.ac.in>
2025-07-23 12:44:23 +0530

    GymCustomerMenu Added
    # Conflicts:
    #	POS/src/com/flipfit/client/GymCustomerMenu.java
    #	POS/src/com/flipfit/client/GymFlipfitApplicationClient.java

---
+++ POS/src/com/flipfit/business/GymCustomerBusinessService.java
@@ -0,0 +1,64 @@
+package com.flipfit.business;
+
+import com.flipfit.beans.GymCustomer;
+
+import java.util.Scanner;
+
+public class GymCustomerBusinessService {
+    public void createProfile(){
+
+        System.out.println("Profile Created");
+    }
+    public void updateProfile(GymCustomer gymCustomer){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter What to Update");
+        System.out.println("1. To Update Your Name");
+        System.out.println("2. To Update Your EmailId");
+        System.out.println("3. To Update Your PassWord");
+        System.out.println("4. To Update Your Phone Number");
+        System.out.println("5. To Update Your Address");
+        try {
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Your New Name");
+                    String newUserName=sc.nextLine();
+                    gymCustomer.setUserName(newUserName);
+                case 2:
+                    System.out.println("Enter Your New EmailId");
+                    String newUserEmailId=sc.nextLine();
+                    gymCustomer.setUserEmail(newUserEmailId);
+                case 3:
+                    System.out.println("Enter Your New PassWord");
+                    String newUserPassWord=sc.nextLine();
+                    gymCustomer.setUserPassword(newUserPassWord);
+                case 4:
+                    System.out.println("Enter Your New Phone Number");
+                    String newUserPhoneNumber=sc.nextLine();
+                    gymCustomer.setPhoneNo(newUserPhoneNumber);
+                case 5:
+                    System.out.println("Enter Your New Address");
+                    String newUserAddress=sc.nextLine();
+                    gymCustomer.setAddress(newUserAddress);
+
+                default:
+
+            }
+        } catch(Exception e){
+            System.out.println("Enter Correct Value");
+        }
+
+    }
+    public String getRole(){
+        System.out.println("Role:");
+        return "";
+    }
+    public void viewAllGyms(){
+
+        System.out.println("Gyms Near Your Area:");
+        System.out.println("Gym-1");
+        System.out.println("Gym-2");
+
+
+    }
+}
--- POS/src/com/flipfit/client/GymCustomerMenu.java
+++ POS/src/com/flipfit/client/GymCustomerMenu.java
@@ -1,7 +1,32 @@
 package com.flipfit.client;
+import  com.flipfit.beans.GymCustomer;
+import com.flipfit.business.GymCustomerBusinessService;
+import java.util.Scanner;
 
 public class GymCustomerMenu {
-    public static void customerPage() {
-        System.out.println("Customer Page loaded Succesfully");
+    public static void CustomerPage(String userName,String userPassword,int roleId){
+        GymCustomer gymUser = new GymCustomer();
+        gymUser.setUserName(userName);
+        gymUser.setUserPassword(userPassword);
+        System.out.println("Welcome"+ gymUser.getUserName()+ "to Flipfit");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter option to Move Next");
+        System.out.println("1. To UpdateProfile");
+		System.out.println("2. To View All Gyms");
+        try {
+            int choice = sc.nextInt();
+            GymCustomerBusinessService gymCustomerBusinessService=new GymCustomerBusinessService();
+            switch (choice) {
+                case 1:
+                    gymCustomerBusinessService.updateProfile(gymUser);
+                case 2:
+                    gymCustomerBusinessService.viewAllGyms();
+                default:
+
+            }
+        } catch(Exception e){
+            System.out.println("Enter Correct Value");
+        }
     }
+
 }

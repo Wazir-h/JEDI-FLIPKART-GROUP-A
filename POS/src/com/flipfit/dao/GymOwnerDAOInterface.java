a3dfc742569f67fe327590f836e92476f9f9e915
trilekya <kallakuritrilekyasri2208@gmail.com>
2025-07-24 09:30:54 +0530

    changes made

--- POS/src/com/flipfit/dao/GymOwnerDAOInterface.java
+++ POS/src/com/flipfit/dao/GymOwnerDAOInterface.java
@@ -1,7 +1,19 @@
 package com.flipfit.dao;
-
+import com.flipfit.beans.GymCentre;
 import com.flipfit.beans.GymOwner;
 
-public interface GymOwnerDAOInterface {
+import java.utils.List;
 
-}
+public interface GymOwnerDAOInterface {
+    public static void AddGymOwner(String username, GymOwner owner);
+    public static void AddGymCenter(String ownerEmail, GymCentre gymCenter);
+    public static void DeleteGymCenter(String ownerEmail, String gymName);
+    public static GymCentre GetGymCenter(String ownerEmail, String gymName);
+    public static void getAllGymCenters(String userName);
+    public static void getAllGyms();
+    public static void getAllOwners();
+    public static GymOwner getGymOwnerDetail(String username);
+    public static boolean authenticateUser(String username, String password);
+    public static boolean userExists(String username);
+
+    }

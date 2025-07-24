ccc62ec6aecc246f48ada29884a609d61de71a98
Sujal Mishra <sujal.mishra.374@gmail.com>
2025-07-24 16:34:15 +0530

    Connection file added

---
+++ POS/src/com/flipfit/Constants/DBconstants.java
@@ -0,0 +1,7 @@
+package com.flipfit.Constants;
+
+public class DBconstants {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/flipfit_db?useSSL=false&serverTimezone=UTC";
+    public static final String DB_USER = "root"; // Your MySQL username
+    public static final String DB_PASSWORD = "password"; // Your MySQL password
+}
---
+++ POS/src/com/flipfit/dao/DBConnection.java
@@ -0,0 +1,15 @@
+package com.flipfit.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.flipfit.Constants.DBconstants.*;
+
+
+public class DBConnection {
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+}

b3f6c0ecc7386f9669c69cc99215c6905f0d78e0
yash-0300 <yashvardhanyadav8@kgpian.iitkgp.ac.in>
2025-07-24 16:49:15 +0530

    Updated SqlQueries.java file

---
+++ POS/src/com/flipfit/constant/SqlQueries.java
@@ -0,0 +1,57 @@
+package com.flipfit.constant;
+
+public class SqlQueries {
+
+    public static final String AUTHENTICATE_USER = "SELECT user_id, user_name, user_email, role_id FROM User WHERE user_email = ? AND user_password = ? AND role_id = ?";
+
+    public static final String FETCH_ALL_USERS_BY_ROLE = "SELECT user_id, user_name, user_email FROM User WHERE role_id = ?";
+
+    public static final String FETCH_ROLE_BY_ID = "SELECT id, name, description FROM Role WHERE id = ?";
+
+    public static final String FETCH_ROLE_BY_NAME = "SELECT id, name, description FROM Role WHERE name = ?";
+
+    public static final String FETCH_ALL_ROLES = "SELECT id, name, description FROM Role";
+
+    public static final String FETCH_ALL_APPROVED_GYMS = "SELECT gym_id, gym_name, gym_center_address, gym_center_phone, slot_count, is_approved, owner_user_id FROM GymCentre WHERE is_approved = TRUE";
+
+    public static final String FETCH_ALL_SLOTS_OF_GYM = "SELECT slot_id, slot_date, slot_time_start, slot_time_end, gym_id FROM Slot WHERE gym_id = ?";
+
+    public static final String INSERT_GYM = "INSERT INTO GymCentre (gym_id, gym_name, gym_center_address, gym_center_phone, slot_count, is_approved, owner_user_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    public static final String FETCH_PENDING_GYM_CENTERS = "SELECT gym_id, gym_name, gym_center_address, gym_center_phone, slot_count, is_approved, owner_user_id FROM GymCentre WHERE is_approved = FALSE";
+
+    public static final String APPROVE_GYM_CENTER = "UPDATE GymCentre SET is_approved = TRUE WHERE gym_id = ?";
+
+    public static final String CHECK_GYMCENTER_APPROVAL = "SELECT gym_id FROM GymCentre WHERE gym_id = ? AND is_approved = TRUE";
+
+    public static final String FETCH_GYMOWNER_DETAILS = "SELECT go.user_id, u.user_name, u.user_email, go.gym_owner_address, go.gym_owner_phone, go.gst_number, go.is_approved FROM GymOwner go JOIN User u ON go.user_id = u.user_id WHERE u.user_email = ?";
+
+    public static final String FETCH_GYMOWNER_ALL_GYMS = "SELECT gc.gym_id, gc.gym_name, gc.gym_center_address, gc.gym_center_phone, gc.slot_count, gc.is_approved, gc.owner_user_id FROM GymCentre gc JOIN GymOwner go ON gc.owner_user_id = go.user_id JOIN User u ON go.user_id = u.user_id WHERE u.user_email = ?";
+
+    public static final String FETCH_GYMOWNER_ALL_APPROVED_GYMS = "SELECT gc.gym_id, gc.gym_name, gc.gym_center_address, gc.gym_center_phone, gc.slot_count, gc.is_approved, gc.owner_user_id FROM GymCentre gc JOIN GymOwner go ON gc.owner_user_id = go.user_id JOIN User u ON go.user_id = u.user_id WHERE u.user_email = ? AND gc.is_approved = TRUE";
+
+    public static final String CHECK_GYMOWNER_APPROVAL = "SELECT go.user_id FROM GymOwner go JOIN User u ON go.user_id = u.user_id WHERE u.user_email = ? AND go.is_approved = TRUE";
+
+    public static final String FETCH_PENDING_OR_APPROVED_GYM_OWNERS = "SELECT go.user_id, u.user_name, u.user_email, go.gym_owner_address, go.gym_owner_phone, go.gst_number, go.is_approved FROM GymOwner go JOIN User u ON go.user_id = u.user_id WHERE go.is_approved = ?";
+
+    public static final String APPROVE_GYM_OWNER = "UPDATE GymOwner SET is_approved = TRUE WHERE user_id = ?";
+
+    public static final String INSERT_SLOT = "INSERT INTO Slot (slot_id, slot_date, slot_time_start, slot_time_end, gym_id) VALUES (?, ?, ?, ?, ?)";
+
+    public static final String INSERT_BOOKING = "INSERT INTO GymBooking (slot_id, user_id, booking_time, is_active) VALUES (?, ?, ?, TRUE)";
+
+    public static final String FETCH_ALL_CUSTOMER_BOOKINGS = "SELECT gb.booking_id, gb.slot_id, gb.user_id, gb.booking_time, gb.is_active FROM GymBooking gb JOIN GymCustomer gc ON gb.user_id = gc.user_id JOIN User u ON gc.user_id = u.user_id WHERE u.user_email = ? AND gb.is_active = ?";
+
+    public static final String CHECK_SLOT_ALREADY_BOOKED = "SELECT gb.booking_id FROM GymBooking gb JOIN User u ON gb.user_id = u.user_id WHERE gb.slot_id = ? AND u.user_email = ? AND DATE(gb.booking_time) = ?";
+
+    public static final String CANCEL_BOOKING = "UPDATE GymBooking SET is_active = FALSE WHERE booking_id = ? AND user_id = (SELECT user_id FROM User WHERE user_email = ?)";
+
+    public static final String REGISTER_NEW_USER = "INSERT INTO User (user_id, user_name, user_email, user_password, role_id) VALUES (?, ?, ?, ?, ?)";
+
+    public static final String REGISTER_NEW_CUSTOMER_DETAILS = "INSERT INTO GymCustomer (user_id, address, phone_no) VALUES (?, ?, ?)";
+
+    public static final String REGISTER_NEW_GYMOWNER_DETAILS = "INSERT INTO GymOwner (user_id, gym_owner_address, gym_owner_phone, gst_number, is_approved) VALUES (?, ?, ?, ?, ?)";
+
+    public static final String MAKE_PAYMENT = "INSERT INTO GymPayment (payment_id, user_id, payment_time) VALUES (?, ?, CURRENT_TIMESTAMP())";
+
+}

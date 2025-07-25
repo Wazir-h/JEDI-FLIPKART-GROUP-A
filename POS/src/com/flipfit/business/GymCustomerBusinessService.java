184bd39679d11d3fe7a24c566723e59c2b89cc76
yash-0300 <yashvardhanyadav8@kgpian.iitkgp.ac.in>
2025-07-25 11:47:55 +0530

    Updated List in GymOwnerService

--- POS/src/com/flipfit/business/GymCustomerBusinessService.java
+++ POS/src/com/flipfit/business/GymCustomerBusinessService.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.List;
 
 public class GymCustomerBusinessService implements GymCustomerBusinessServiceInterface{
 
